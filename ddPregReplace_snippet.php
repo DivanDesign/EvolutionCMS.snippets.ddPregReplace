b64116c63f493051ea5ac4015a5da440aa42a758
Ilyas Ronef <ilyas@divandesign.biz>
2018-11-25 16:26:18 +0300

    Prerelease 1.1

--- ddPregReplace_snippet.php
+++ ddPregReplace_snippet.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ddPregReplace
- * @version 1.0.1 (2013-09-17)
+ * @version 1.1 (2018-11-25)
  * 
  * @desc Searches “inputString” for matches to “pattern” and replaces them with “replace”.
  * 
@@ -11,10 +11,10 @@
  * @param $inputString {string} — The input string. @required
  * @param $inputString_docField {string} — The name of the document field/TV which value is required to get. If the parameter is passed then the input string will be taken from the field/TV and “inputString” will be ignored. Default: —.
  * @param $inputString_docId {integer} — ID of the document which field/TV value is required to get. “inputString_docId” equals the current document id since “inputString_docId” is unset. Default: —.
- * @param $pattern {string} — Шаблон для замены (регулярное выражение). @required
+ * @param $pattern {string_regex} — Шаблон для замены (регулярное выражение). @required
  * @param $replace {string} — На что надо заменить. Default: ''.
  * 
- * @copyright 2013 DivanDesign {@link http://www.DivanDesign.biz }
+ * @copyright 2013–2018 DivanDesign {@link http://www.DivanDesign.biz }
  */
 
 //Include (MODX)EvolutionCMS.libraries.ddTools
