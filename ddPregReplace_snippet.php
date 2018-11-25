da06165250d6977b5316c0681584d9981c495d6e
Ilyas Ronef <ilyas@divandesign.biz>
2018-11-25 13:17:42 +0300

    + Added an ability to get the input string from some document field/TV.

--- ddPregReplace_snippet.php
+++ ddPregReplace_snippet.php
@@ -9,6 +9,8 @@
  * @uses (MODX)EvolutionCMS.libraries.ddTools >= 0.23.
  * 
  * @param $inputString {string} — The input string. @required
+ * @param $inputString_docField {string} — The name of the document field/TV which value is required to get. If the parameter is passed then the input string will be taken from the field/TV and “inputString” will be ignored. Default: —.
+ * @param $inputString_docId {integer} — ID of the document which field/TV value is required to get. “inputString_docId” equals the current document id since “inputString_docId” is unset. Default: —.
  * @param $pattern {string} — Шаблон для замены (регулярное выражение). @required
  * @param $replace {string} — На что надо заменить. Default: ''.
  * 
@@ -26,6 +28,14 @@ extract(ddTools::verifyRenamedParams(
 	]
 ));
 
+//If we need to get the input string from some document field/TV
+if (isset($inputString_docField)){
+	$inputString = ddTools::getTemplateVarOutput(
+		[$inputString_docField],
+		$inputString_docId
+	);
+	$inputString = $inputString[$inputString_docField];
+}
 
 $replace = isset($replace) ? $replace : '';
 
